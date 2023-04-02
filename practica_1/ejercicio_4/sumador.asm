34fa523da7711ceaa06557da86be5671790cf346
martinykrz <ymartin014@gmail.com>
2023-04-02 16:14:32 -0300

    flag stuff

--- practica_1/ejercicio_4/sumador.asm
+++ practica_1/ejercicio_4/sumador.asm
@@ -1,8 +1,12 @@
 section .text
  global _start
  _start:
-    mov     al,5
-    add     al,3
+    mov     bl,255  
+    add     bl,1        ;Carry Flag, Zero Flag
+    mov     rbx,0xffffffffffffffff
+    add     rbx,1 
+    add     rbx,-1
+    add     rbx,0
     syscall
     
     mov     rax,60
