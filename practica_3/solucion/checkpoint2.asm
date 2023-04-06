3593186662da8be56828e99d178ecfa17dbe51e9
martinykrz <ymartin014@gmail.com>
2023-04-06 14:38:02 -0300

    init practica_3

---
+++ practica_3/solucion/checkpoint2.asm
@@ -0,0 +1,68 @@
+extern sumar_c
+extern restar_c
+;########### SECCION DE DATOS
+section .data
+
+;########### SECCION DE TEXTO (PROGRAMA)
+section .text
+
+;########### LISTA DE FUNCIONES EXPORTADAS
+
+global alternate_sum_4
+global alternate_sum_4_simplified
+global alternate_sum_8
+global product_2_f
+global alternate_sum_4_using_c
+
+;########### DEFINICION DE FUNCIONES
+; uint32_t alternate_sum_4(uint32_t x1, uint32_t x2, uint32_t x3, uint32_t x4);
+; registros: x1[?], x2[?], x3[?], x4[?]
+alternate_sum_4:
+	;prologo
+	; COMPLETAR
+
+	;recordar que si la pila estaba alineada a 16 al hacer la llamada
+	;con el push de RIP como efecto del CALL queda alineada a 8
+
+	;epilogo
+	; COMPLETAR
+	ret
+
+; uint32_t alternate_sum_4_using_c(uint32_t x1, uint32_t x2, uint32_t x3, uint32_t x4);
+; registros: x1[rdi], x2[rsi], x3[rdx], x4[rcx]
+alternate_sum_4_using_c:
+	;prologo
+	push rbp ; alineado a 16
+	mov rbp,rsp
+
+	; COMPLETAR
+
+	;epilogo
+	pop rbp
+	ret
+
+
+
+; uint32_t alternate_sum_4_simplified(uint32_t x1, uint32_t x2, uint32_t x3, uint32_t x4);
+; registros: x1[?], x2[?], x3[?], x4[?]
+alternate_sum_4_simplified:
+	ret
+
+
+; uint32_t alternate_sum_8(uint32_t x1, uint32_t x2, uint32_t x3, uint32_t x4, uint32_t x5, uint32_t x6, uint32_t x7, uint32_t x8);
+; registros y pila: x1[?], x2[?], x3[?], x4[?], x5[?], x6[?], x7[?], x8[?]
+alternate_sum_8:
+	;prologo
+
+	; COMPLETAR
+
+	;epilogo
+	ret
+
+
+; SUGERENCIA: investigar uso de instrucciones para convertir enteros a floats y viceversa
+;void product_2_f(uint32_t * destination, uint32_t x1, float f1);
+;registros: destination[?], x1[?], f1[?]
+product_2_f:
+	ret
+
---
+++ practica_3/solucion/checkpoint3.asm
@@ -0,0 +1,56 @@
+
+;########### SECCION DE DATOS
+section .data
+
+;########### SECCION DE TEXTO (PROGRAMA)
+section .text
+
+;########### LISTA DE FUNCIONES EXPORTADAS
+global complex_sum_z
+global packed_complex_sum_z
+global product_9_f
+
+;########### DEFINICION DE FUNCIONES
+;extern uint32_t complex_sum_z(complex_item *arr, uint32_t arr_length);
+;registros: arr[?], arr_length[?]
+complex_sum_z:
+	;prologo
+
+	mov ecx, 99 ; carga la cantidad de iteraciones a hacer al contador de vueltas
+	.cycle:     ; etiqueta a donde retorna el ciclo que itera sobre arr
+	loop .cycle ; decrementa ecx y si es distinto de 0 salta a .cycle
+
+	;epilogo
+	ret
+
+;extern uint32_t packed_complex_sum_z(packed_complex_item *arr, uint32_t arr_length);
+;registros: arr[?], arr_length[?]
+packed_complex_sum_z:
+	ret
+
+
+;extern void product_9_f(uint32_t * destination
+;, uint32_t x1, float f1, uint32_t x2, float f2, uint32_t x3, float f3, uint32_t x4, float f4
+;, uint32_t x5, float f5, uint32_t x6, float f6, uint32_t x7, float f7, uint32_t x8, float f8
+;, uint32_t x9, float f9);
+;registros y pila: destination[rdi], x1[?], f1[?], x2[?], f2[?], x3[?], f3[?], x4[?], f4[?]
+;	, x5[?], f5[?], x6[?], f6[?], x7[?], f7[?], x8[?], f8[?],
+;	, x9[?], f9[?]
+product_9_f:
+	;prologo
+	push rbp
+	mov rbp, rsp
+
+	;convertimos los flotantes de cada registro xmm en doubles
+	; COMPLETAR
+
+	;multiplicamos los doubles en xmm0 <- xmm0 * xmm1, xmmo * xmm2 , ...
+	; COMPLETAR
+
+	; convertimos los enteros en doubles y los multiplicamos por xmm0.
+	; COMPLETAR
+
+	; epilogo
+	pop rbp
+	ret
+
