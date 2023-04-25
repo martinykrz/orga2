075e5ada7ebfcef78bb92b0e85a6147a52aaeeb5
martinykrz <84545925+martinykrz@users.noreply.github.com>
2023-04-25 19:39:47 -0300

    char* is const string

--- pre-parcial/ejercicios_asm/ejercicio6_c.c
+++ pre-parcial/ejercicios_asm/ejercicio6_c.c
@@ -7,15 +7,15 @@ char* cesar(char* s, uint8_t n) {
     while (s[i] != '\0') {
         i++;
     }
-    char* res = malloc(i*sizeof(char*));
-    for (int j=0; j <= i; j++) {
+    char* res = malloc((i+1)*sizeof(char));
+    for (int j=0; j < i; j++) {
         res[j] = (char)((uint8_t)s[j] + (n % 26));
     }
     return res;
 }
 
 int main() {
-    char* t1 = "CASA";
+    char* t1 = "CASA"; //String constante
     char* t2 = "CALABAZA";
     char* res1 = cesar(t1, 3);
     char* res2 = cesar(t2, 7);
