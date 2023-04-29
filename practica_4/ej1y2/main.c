a4728825cf75a5f39d00e1af7718ef9bf9705a8e
martinykrz <ymartin014@gmail.com>
2023-04-29 00:37:12 -0300

    test with one

--- practica_4/ej1y2/main.c
+++ practica_4/ej1y2/main.c
@@ -24,25 +24,26 @@ uint32_t shuffle_int(uint32_t min, uint32_t max){
 
 int main (void){
 	/* Acá pueden realizar sus propias pruebas */
-    for (int i=0; i < 100; i++) {
-        uint8_t* bytes = (uint8_t*) malloc(16);
-        shuffle(256);
-        uint8_t m = shuffle_int(0,16);
-        uint8_t n = shuffle_int(0,16);
-        printf("%d: (%d, %d)\n", i, n, m);
-        printf("[");
-        for (int j=0; j < 16; j++){
-            bytes[j] = x[j];
-            printf("%d, ", x[j]);
-        }
-        printf("]\n");
-        invertirBytes_asm(bytes, n, m);
-        printf("[");
-        for (int k=0; k < 16; k++){
-            printf("%d, ", bytes[k]);
-        }
-        printf("]\n");
-        free(bytes);    
-    }
+    /* for (int i=0; i < 5; i++) { */
+        /* uint32_t array_size = shuffle_int(1,20); */
+        uint32_t array_size = 1;
+        data_t* test_data = (data_t*) malloc(sizeof(data_t)*array_size);
+        /* for(uint32_t j = 0; j < array_size; j++){ */
+            shuffle(16);
+            printf("x(%d): ", 0);
+            for (int i=0 ; i < ROLL_LENGTH; i++) {
+                printf("%d, ", x[i]);
+            }
+            printf("\n");
+            for (int k = 0; k < 8; k++){
+                test_data[0].a[k] = x[k];
+                test_data[0].b[k] = x[k+8];
+                test_data[0].c[k] = ((uint32_t) test_data[0].a[k] + (uint32_t) test_data[0].b[k])*8;
+            }
+        /* } */
+        printf("asm -> %d\n", checksum_asm(test_data, array_size));
+        printf("c -> %d\n", checksum_c(test_data, array_size));
+        free(test_data);
+    /* } */
     return 0;
 }
