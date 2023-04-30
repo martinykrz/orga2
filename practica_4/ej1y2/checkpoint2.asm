22f9130598da99ad00fa19de4d98368eec19d30d
martinykrz <ymartin014@gmail.com>
2023-04-30 11:16:02 -0300

    done

--- practica_4/ej1y2/checkpoint2.asm
+++ practica_4/ej1y2/checkpoint2.asm
@@ -10,40 +10,56 @@ checksum_asm:
     mov rbp, rsp
 
     xor rax, rax
+    xor rcx, rcx
+    xor rdx, rdx
 
     pxor xmm0, xmm0
     pxor xmm1, xmm1
     pxor xmm2, xmm2
     pxor xmm3, xmm3
-    pxor xmm4, xmm4
-    pxor xmm5, xmm5
+    
+    .iter:
+        cmp rcx, rsi
+        je .end
+    
+        movdqu xmm0, [rdi + rdx] ; a
+        movdqu xmm1, [rdi + rdx + 0x10] ; b
+        movdqu xmm2, [rdi + rdx + 0x20] ; c[0:15]
+        movdqu xmm3, [rdi + rdx + 0x30] ; c[16:32]
 
-    movdqu xmm0, [rdi] ; a
-    movdqu xmm1, [rdi + 0x10] ; b
-    movdqu xmm2, [rdi + 0x20] ; c[0:15]
-    movdqu xmm3, [rdi + 0x30] ; c[16:32]
+        paddw xmm0, xmm1 ; a + b
+        psllw xmm0, 3 ; (a + b) * 8
 
-    paddw xmm0, xmm1
-    psllw xmm0, 3
+        pxor xmm4, xmm4
+        pxor xmm5, xmm5
 
-    packusdw xmm4, xmm2
-    packusdw xmm3, xmm5
-    paddw xmm3, xmm4
+        packusdw xmm2, xmm4 ; packed on front
+        packusdw xmm5, xmm3 ; packed on back
+        paddw xmm2, xmm5 ; packed c from double -> word
 
-    pcmpeqw xmm0, xmm3
+        pcmpeqw xmm0, xmm2
 
-    xor rdx, rdx
-    xor rcx, rcx
-    movq rdx, xmm0
-    psrldq xmm0, 8
-    movq rcx, xmm0
+        xor r8, r8
+        xor r9, r9
+        mov r9, rcx
+        mov rcx, 0x10
+        .check:
+            pextrb r8, xmm0, 0
+            cmp byte r8, 0xff
+            jne .error
+            psrldq xmm0, 1
+        loop .check
+        mov rcx, r9
 
-    cmp rdx, rcx
-    je .equal
-    jmp .end
+        .equal:
+            mov rax, 1
+            inc rcx
+            add rdx, 0x40
+            jmp .iter
 
-    .equal:
-        inc rax
+        .error:
+            xor rax, rax
+            jmp .end
 
     .end:
         pop rbp
--- practica_4/ej1y2/main.c
+++ practica_4/ej1y2/main.c
@@ -24,26 +24,33 @@ uint32_t shuffle_int(uint32_t min, uint32_t max){
 
 int main (void){
 	/* Acá pueden realizar sus propias pruebas */
-    /* for (int i=0; i < 5; i++) { */
-        /* uint32_t array_size = shuffle_int(1,20); */
-        uint32_t array_size = 1;
+    for (int i=0; i < 100; i++) {
+        uint32_t array_size = shuffle_int(1,20);
         data_t* test_data = (data_t*) malloc(sizeof(data_t)*array_size);
-        /* for(uint32_t j = 0; j < array_size; j++){ */
-            shuffle(16);
-            printf("x(%d): ", 0);
-            for (int i=0 ; i < ROLL_LENGTH; i++) {
-                printf("%d, ", x[i]);
-            }
-            printf("\n");
+        for(uint32_t j = 0; j < array_size; j++){
+            shuffle(1000);
             for (int k = 0; k < 8; k++){
-                test_data[0].a[k] = x[k];
-                test_data[0].b[k] = x[k+8];
-                test_data[0].c[k] = ((uint32_t) test_data[0].a[k] + (uint32_t) test_data[0].b[k])*8;
+                test_data[j].a[k] = x[k];
+                test_data[j].b[k] = x[k+8];
+                /* uint32_t value = shuffle_int(5,10); */
+                uint32_t value = 8;
+                test_data[j].c[k] = ((uint32_t) test_data[j].a[k] + (uint32_t) test_data[j].b[k])*value;
             }
-        /* } */
-        printf("asm -> %d\n", checksum_asm(test_data, array_size));
-        printf("c -> %d\n", checksum_c(test_data, array_size));
+        }
+        uint32_t asm_ = checksum_asm(test_data, array_size);
+        uint32_t c_ = checksum_c(test_data, array_size);
+        if (asm_ != c_) {
+            printf("%d -> asm: %d, c: %d\n", i, asm_, c_);
+            printf("Datos: array_size -> %d\n", array_size);
+            for(uint32_t j = 0; j < array_size; j++){
+                for (int k = 0; k < 8; k++){
+                    uint16_t suma = test_data[j].a[k]+test_data[j].b[k];
+                    printf("%d -> a: %-8d b: %-8d c: %-8dres: %d\n", k, test_data[j].a[k], test_data[j].b[k], test_data[j].c[k], suma*8);
+                }
+                printf("---------------------\n");
+            }
+        }
         free(test_data);
-    /* } */
+    }
     return 0;
 }
