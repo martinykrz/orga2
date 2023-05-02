bc7532b72dc070e98b00dbb555c6b07a76244887
martinykrz <ymartin014@gmail.com>
2023-05-02 14:11:36 -0300

    cooking

--- parcial1/ej2/ej2.asm
+++ parcial1/ej2/ej2.asm
@@ -22,40 +22,83 @@ miraQueCoincidencia:
     .equal:
         push r12
         push r13
+        push r14
+        push r15
 
-        xor r9, r9
         pxor xmm0, xmm0
         pxor xmm1, xmm1
         pxor xmm2, xmm2
         pxor xmm3, xmm3
 
-        movdqu xmm0, r8
+        movq xmm0, r8
+        xor r14, r14
 
         .cycle:
+            cmp r14, byte 0x8
+            je .end
+            ; je .operation
+            xor r9, r9
             pextrb r9, xmm0, 0
             psrldq xmm0, 1
-            movdqu xmm1, r9 ; if blue
-            movdqu xmm2, r9 ; if green
-            movdqu xmm3, r9 ; if red
-            mov r12, r9 ; if a_1
-            mov r13, r9 ; if a_2
+            cmp r14, byte 0x0
+            je .blue
+            cmp r14, byte 0x4
+            je .blue
+            cmp r14, byte 0x1
+            je .green
+            cmp r14, byte 0x5
+            je .green
+            cmp r14, byte 0x2
+            je .red
+            cmp r14, byte 0x6
+            je .red
+            cmp r14, byte 0x3
+            je .transparency_a
+            cmp r14, byte 0x7
+            je .transparency_b
+         
+        .blue:
+            movq xmm1, r9
+            inc r14
             jmp .cycle
         
-        .operacion:
-            call cuentas
-    ;TODO
-    ; En un ciclo, 
-    ;   * Almacenar en un xmm A y B
-    ;   * Comparar a 32 bits
-    ;   * Si son iguales,
-    ;       - Guardar el ultimo byte
-    ;       - Borrar el ultimo byte
-    ;       - Hacer el calculo y reemplazar los 24 bits restantes con el res
-    ;       - Sumarle o devolcerle el byte guardado
-    ;   * Sino,
-    ;       - Transformar cada byte (a tambien?) en 255 (0xff)
-    ;   * Almacenar el resultado en rcx en el lugar que corresponda
-    ;   * Iterar de vuelta
+        .green:
+            movq xmm2, r9
+            inc r14
+            jmp .cycle
+            
+        .red:
+            movq xmm3, r9
+            inc r14
+            jmp .cycle
 
-    pop rbp
-    ret
+        .transparency_a:
+            mov r12, r9
+            inc r14
+            jmp .cycle
+
+        .transparency_b:
+            mov r13, r9
+            inc r14
+            jmp .cycle
+
+        ; .operacion:
+            ; call cuentas
+    
+    .end:
+        pop rbp
+        ret
+
+;TODO
+; En un ciclo, 
+;   * Almacenar en un xmm A y B
+;   * Comparar a 32 bits
+;   * Si son iguales,
+;       - Guardar el ultimo byte
+;       - Borrar el ultimo byte
+;       - Hacer el calculo y reemplazar los 24 bits restantes con el res
+;       - Sumarle o devolcerle el byte guardado
+;   * Sino,
+;       - Transformar cada byte (a tambien?) en 255 (0xff)
+;   * Almacenar el resultado en rcx en el lugar que corresponda
+;   * Iterar de vuelta
