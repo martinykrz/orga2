25d0d96fa76d7fb35298eab9d427e41bf202f89f
martinykrz <ymartin014@gmail.com>
2023-05-01 21:12:59 -0300

    beginning

--- practica_4/filters/Offset_asm.asm
+++ practica_4/filters/Offset_asm.asm
@@ -1,10 +1,15 @@
 
 global Offset_asm
 
-
+; Offset_asm(uint8_t *src, uint8_t *dst, int width, int height, int src_row_size, int dst_row_size)
+; src[rdi], dstp[rsi], width[rdx], height[rcx], src_row_size[r8], dst_row_size[r9]
 Offset_asm:
+    push rbp
+    mov rbp, rsp
 
+    pxor xmm0, xmm0
+
+    movlqu xmm0, [rdi]
+
+    pop rbp
 	ret
-	
-
-
