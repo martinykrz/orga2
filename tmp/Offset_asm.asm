9e18a263fef78a07bcc447d6b62e31e4ac6b231b
martinykrz <ymartin014@gmail.com>
2023-05-14 15:58:24 -0300

    baby steps

--- tmp/Offset_asm.asm
+++ tmp/Offset_asm.asm
@@ -1,15 +1,35 @@
-
+extern malloc
 global Offset_asm
 
-; Offset_asm(uint8_t *src, uint8_t *dst, int width, int height, int src_row_size, int dst_row_size)
-; src[rdi], dstp[rsi], width[rdx], height[rcx], src_row_size[r8], dst_row_size[r9]
+; Offset_asm(
+;   uint8_t *src, [rdi]
+;   uint8_t *dst, [rsi]
+;   int width, [rdx]
+;   int height, [rcx]
+;   int src_row_size, [r8]
+;   int dst_row_size [r9]
+;)
+; sizeof(bgra_t) = 4 => (value, offset): (b, 0), (g, 1), (r, 2), (a, 3)
 Offset_asm:
     push rbp
     mov rbp, rsp
 
     ; bgra_t (*src_matrix)[(src_row_size+3)/4] = (bgra_t (*)[(src_row_size+3)/4]) src
-    
     ; bgra_t (*dst_matrix)[(dst_row_size+3)/4] = (bgra_t (*)[(dst_row_size+3)/4]) dst
+    call init
+    call offset
 
     pop rbp
 	ret
+
+init:
+    ; src_row_size+3/4
+    add r8, 0x3
+    shr r8, 0x2
+    ; dst_row_size+3/4
+    add r9, 0x3
+    shr r9, 0x2
+    
+
+offset:
+    
