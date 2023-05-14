e987abed1ac3246a14a465bf83e16baba6cafab0
martinykrz <ymartin014@gmail.com>
2023-05-14 00:51:28 -0300

    dont know

--- tmp/Offset_asm.asm
+++ tmp/Offset_asm.asm
@@ -7,7 +7,9 @@ Offset_asm:
     push rbp
     mov rbp, rsp
 
-    ;TODO
+    ; bgra_t (*src_matrix)[(src_row_size+3)/4] = (bgra_t (*)[(src_row_size+3)/4]) src
+    
+    ; bgra_t (*dst_matrix)[(dst_row_size+3)/4] = (bgra_t (*)[(dst_row_size+3)/4]) dst
 
     pop rbp
 	ret
