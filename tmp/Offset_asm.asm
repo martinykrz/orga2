45e795230b80d541526d0ba1acb76e4d4f8cc940
martinykrz <ymartin014@gmail.com>
2023-05-22 17:46:08 -0300

    still dk

--- tmp/Offset_asm.asm
+++ tmp/Offset_asm.asm
@@ -14,38 +14,17 @@ Offset_asm:
     mov rbp, rsp
 
     push r12
-    push r13
-    push r14
+    xor r12, r12
 
-    ; init values of i,j 
-    mov r12, rdx
-    add r12, 0x4
-    imul r12, 0x8
-
-    ; height + 8
-    mov r13, rdx
-    imul r13, 0x8
-
-    ; widht + 8
-    mov r14, rdx
-    add r14, 0x8
-
-    ; source
     pxor xmm1, xmm1
-    pxor xmm2, xmm2
-    pxor xmm3, xmm3
-    movdqu xmm1, [rdi + r12 + r13 + 0x0] ; blue
-    movdqu xmm2, [rdi + r12 + r14 + 0x1] ; green
-    movdqu xmm3, [rdi + r12 + r13 + r14 + 0x2] ; red
-
-    ; destination
-    pxor xmm4, xmm4
-    pxor xmm5, xmm5
-    pxor xmm6, xmm6
-    movdqu xmm4, [rsi + r12 + 0x0] ; blue
-    movdqu xmm5, [rsi + r12 + 0x1] ; green
-    movdqu xmm6, [rsi + r12 + 0x2] ; red
-
-    pop rbp
-    ret
+    .iter:
+        cmp r12, byte 0xff
+        jg .end
+        movdqu xmm1, [rdi + r12]
+        add r12, 0x4
+        jmp .iter
+    .end:
+        pop r12
+        pop rbp
+        ret
 
