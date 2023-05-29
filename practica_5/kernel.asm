b26257b01584c64029d1432f400ae1f2ad007b08
martinykrz <ymartin014@gmail.com>
2023-05-28 22:42:48 -0300

    wip: stack, screen_layout

--- practica_5/kernel.asm
+++ practica_5/kernel.asm
@@ -11,10 +11,11 @@ global start
 ; COMPLETAR - Agreguen declaraciones extern según vayan necesitando
 extern GDT_DESC
 extern A20_enable
+extern screen_draw_layout
 
 ; COMPLETAR - Definan correctamente estas constantes cuando las necesiten
-;%define CS_RING_0_SEL ??   
-;%define DS_RING_0_SEL ??   
+%define CS_RING_0_SEL  0x08  
+%define DS_RING_0_SEL  0x18  
 
 
 BITS 16
@@ -64,19 +65,27 @@ start:
     ; COMPLETAR - Saltar a modo protegido (far jump)
     ; (recuerden que un far jmp se especifica como jmp CS_selector:address)
     ; Pueden usar la constante CS_RING_0_SEL definida en este archivo
+    jmp CS_RING_0_SEL:modo_protegido
 
 BITS 32
 modo_protegido:
-    ; COMPLETAR - A partir de aca, todo el codigo se va a ejectutar en modo protegido
+    ; A partir de aca, todo el codigo se va a ejectutar en modo protegido
     ; Establecer selectores de segmentos DS, ES, GS, FS y SS en el segmento de datos de nivel 0
     ; Pueden usar la constante DS_RING_0_SEL definida en este archivo
+    mov ax, DS_RING_0_SEL
+    mov ds, ax
+    mov es, ax
+    mov gs, ax
+    mov fs, ax
+    mov ss, ax
 
     ; COMPLETAR - Establecer el tope y la base de la pila
 
-    ; COMPLETAR - Imprimir mensaje de bienvenida - MODO PROTEGIDO
+    ; Imprime mensaje de bienvenida - MODO PROTEGIDO
+    print_text_pm start_pm_msg, start_pm_len, 1, 0, 0
 
     ; COMPLETAR - Inicializar pantalla
-    
+    call screen_draw_layout
    
     ; Ciclar infinitamente 
     mov eax, 0xFFFF
