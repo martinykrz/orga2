efa47d0a42780b5a8ee49d78f091ad1dd5719058
martinykrz <ymartin014@gmail.com>
2023-06-12 22:20:19 -0300

    wip: hasta idle funciona (creo), despues se pone raro

--- practica_8/kernel.asm
+++ practica_8/kernel.asm
@@ -24,13 +24,13 @@ extern mmu_unmap_page
 extern copy_page
 extern tss_init
 extern tasks_screen_draw
-; extern sched_init
-; extern tasks_init
-;
+extern tss_create_user_task
+extern sched_init
+extern tasks_init
+
 ; COMPLETAR - Definan correctamente estas constantes cuando las necesiten
 %define CS_RING_0_SEL  0x08  
 %define DS_RING_0_SEL  0x18  
-
 
 BITS 16
 ;; Saltear seccion de datos
@@ -138,22 +138,21 @@ modo_protegido:
     shl ax, 3 
     ltr ax
 
-    ; jmp 0x60:0
-     
-    ; ; Init Scheduler
+    ; Init Scheduler
     ; call sched_init
     ; call tasks_init
-    ;
-    ; ; Aceleramos el PIT (Programmable Interrupt Timer)
-    ; ; El PIT corre a 1193182Hz.
-    ; ; Cada iteracion del clock decrementa un contador interno, cuando este llega
-    ; ; a cero se emite la interrupcion. El valor inicial es 0x0 que indica 65536,
-    ; ; es decir 18.206Hz
+
+    ; Aceleramos el PIT (Programmable Interrupt Timer)
+    ; El PIT corre a 1193182Hz.
+    ; Cada iteracion del clock decrementa un contador interno, cuando este llega
+    ; a cero se emite la interrupcion. El valor inicial es 0x0 que indica 65536,
+    ; es decir 18.206Hz
     ; mov ax, 500
     ; out 0x40, al
     ; rol ax, 8
     ; out 0x40, al
-    ;
+
+    ; Activamos interrupciones
     sti
 
     ; Ciclar infinitamente 
