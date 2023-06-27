08c0c4228af0ea4a12b57ee1a173cdeb542b8738
martinykrz <ymartin014@gmail.com>
2023-06-27 12:04:09 -0300

    DONE :D

--- tmp/Offset_asm.asm
+++ tmp/Offset_asm.asm
@@ -9,7 +9,6 @@ global Offset_asm
 ;   int dst_row_size [r9]
 ;)
 ; sizeof(bgra_t) = 4 => (value, offset): (b, 0), (g, 1), (r, 2), (a, 3)
-; [rdi + i*src_row_size + j]
 Offset_asm:
     push rbp
     mov rbp, rsp
@@ -21,103 +20,120 @@ Offset_asm:
     push r14
     push r15
 
-    xor r12, r12
-    add r12, 0x8 ; i = 8
+    call making_masks
 
-    sub rcx, 0x8 ; height - 8
-    sub rdx, 0x8 ; width - 8
+    pxor xmm5, xmm5
+    pxor xmm6, xmm6
+    pxor xmm7, xmm7
 
-    pxor xmm4, xmm4 ; Blue-Mask
-    pxor xmm5, xmm5 ; Green-Mask
-    pxor xmm6, xmm6 ; Red-Mask
-    pxor xmm7, xmm7 ; Alpha
-
-    mov r11, 0xFF
-    movq xmm4, r11
-    punpckldq xmm4, xmm4
-    punpckldq xmm4, xmm4
-    punpckldq xmm4, xmm4
+    xor r10, r10 ; dst iter
+    xor r11, r11 ; dst_row_size
+    xor r12, r12 ; width
+    xor r13, r13 ; height
+    xor r14, r14
+    xor r15, r15
      
-    movdqa xmm5, xmm4
-    pslldq xmm5, 1
+    mov r11, r9
+    shl r11, 3 ; dst_row_size * 8
+    mov r12, rdx
+    mov r13, rcx
+    sub r12, 16 ; subtract border
+    sub r13, 16 ; subtract border
 
-    movdqa xmm6, xmm5
-    pslldq xmm6, 1
+    xor rax, rax
+    .top_border:
+        ; 8 filas negras
+        movaps [rsi + r10], xmm4
+        add r10, 16
+        add rax, 2
+        cmp rax, r8
+        jne .top_border
 
-    movdqa xmm7, xmm6
-    pslldq xmm7, 1
+    .offset_filter:
+        ; left 8 black px
+        movaps [rsi + r10], xmm4
+        add r10, 16
+        movaps [rsi + r10], xmm4
+        add r10, 16
 
-    .iter_i:
-        cmp r12, rcx
-        je .end
+        xor rax, rax ; px counter
+        .main:
+            movdqu xmm0, xmm4 ; res
+            movdqu xmm5, [rdi + r10 + 32] ; src[i][j+8]
+            mov r14, r10
+            add r14, r11 
+            movdqa xmm6, [rdi + r14] ; src[i+8][j]
+            add r14, 32
+            movdqa xmm7, [rdi + r14] ; src[i+8][j+8]
 
-        xor r13, r13
-        add r13, 0x8 ; j = 8
-
-        ; i + 8
-        mov r14, r12
-        add r14, 0x8
-        imul r14, r8
-
-        ; dst
-        pxor xmm0, xmm0
-
-        ; src
-        pxor xmm1, xmm1 ; Blue
-        pxor xmm2, xmm2 ; Green
-        pxor xmm3, xmm3 ; Red
-
-        .iter_j:
-            cmp r13, rdx
-            je .i_idx
-
-            xor r10, r10
-            mov r10, r12
-            imul r10, r9
-            add r10, r13
+            ; Offset itself
+            pand xmm5, xmm1 ; blue
+            pand xmm6, xmm2 ; green
+            pand xmm7, xmm3 ; red
              
-            ; j + 8
-            mov r15, r13
-            add r15, 0x8
-             
-            add r14, r13
-            movdqu xmm1, [rdi + r14] ; src[i+8][j]
-            sub r14, r13
+            ; Make result
+            por xmm0, xmm5
+            por xmm0, xmm6
+            por xmm0, xmm7
 
-            add r15, r12
-            movdqu xmm2, [rdi + r15] ; src[i][j+8]
-            sub r15, r12
+            ; Send to dst
+            movaps [rsi + r10], xmm0
+            add r10, 16
 
-            add r14, r15
-            movdqu xmm3, [rdi + r14] ; src[i+8][j+8]
-            sub r14, r15
+            add rax, 4
+            cmp rax, r12
+            jne .main
 
-            ; Apply maks to extract colours
-            pand xmm1, xmm4
-            pand xmm2, xmm5
-            pand xmm3, xmm6
+        ; right 8 black px
+        movaps [rsi + r10], xmm4
+        add r10, 16
+        movaps [rsi + r10], xmm4
+        add r10, 16
+         
+        inc r15
+        cmp r15, r13
+        jne .offset_filter
 
-            paddq xmm0, xmm1 ; Add Blue
-            paddq xmm0, xmm2 ; Add Green
-            paddq xmm0, xmm3 ; Add Red
-            paddq xmm0, xmm7 ; Add Alpha
-             
-            movdqa [rsi], xmm0 ; Apply Offset
+    xor rax, rax
+    .bottom_border:
+        ; 8 filas negras
+        movaps [rsi + r10], xmm4
+        add r10, 16
+        add rax, 2
+        cmp rax, r8
+        jne .bottom_border
 
-            inc r13
-            jmp .iter_j
-             
-        .i_idx:
-            inc r12
-            jmp .iter_i
+    pop r15
+    pop r14
+    pop r13
+    pop r12
+    pop r11
+    pop r10
+     
+    pop rbp
+    ret
 
-    .end:
-        pop r15
-        pop r14
-        pop r13
-        pop r12
-        pop r11
-        pop r10
-        pop rbp
-        ret
+making_masks:
+    xor r10, r10
+    pxor xmm1, xmm1 ; Blue-Mask
+    pxor xmm2, xmm2 ; Green-Mask
+    pxor xmm3, xmm3 ; Red-Mask
+    pxor xmm4, xmm4 ; Alpha
+
+    mov r10, 0xFF
+    movq xmm1, r10
+    punpckldq xmm1, xmm1
+    punpckldq xmm1, xmm1
+    punpckldq xmm1, xmm1
+     
+    movdqa xmm2, xmm1
+    pslldq xmm2, 1
+
+    movdqa xmm3, xmm2
+    pslldq xmm3, 1
+
+    movdqa xmm4, xmm3
+    pslldq xmm4, 1
+
+    ret
 
